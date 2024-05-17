810310bee1be5c25f8ea557a4e58021e21342208
jooeelMarin <joel.marin@insjoanamigo.cat>
2024-05-17 14:36:09 +0200

    aa

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ let usuari, contrasenya, seccio_origen, mapa, geoID;
 let storage = window.localStorage;
 let scriptURL = "https://script.google.com/macros/s/AKfycbwO6FnBnaBmxrzPRyLDhV8D6pW7Pe6V8-SYIdSHZXMmg8JqjJwQz8H8zA8wl3U6u2pW/exec";
 
+
+
+
+let pixels = 24;    // nombre de píxels de la forma
+let mida = 2 * pixels;    // mida de visualització en el mapa
+let ref_vertical = mida / 2;    // distància vertical des del punt superior de la icona fins al punt de la localització
+let color = "yellow";
+let path = "M12,1C10.89,1 10,1.9 10,3C10,4.11 10.89,5 12,5C13.11,5 14,4.11 14,3A2,2 0 0,0 12,1M10,6C9.73,6 9.5,6.11 9.31,6.28H9.3L4,11.59L5.42,13L9,9.41V22H11V15H13V22H15V9.41L18.58,13L20,11.59L14.7,6.28C14.5,6.11 14.27,6 14,6";    // cadena de text de la forma
+let cadenaSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ' + pixels + ' ' + pixels + '"><path d="' + path + '" fill="' + color + '" /></svg>';    // construcció de l'element SVG
+let icona = encodeURI("data:image/svg+xml," + cadenaSVG);    // codificació d'espais i caràcters especials per formar una URL vàlida
+let icon = L.icon({    // propietats de la icona
+    iconUrl: icona,    // URL de la forma
+    iconSize: [mida, mida],    // mida de la icona
+    iconAnchor: [mida / 2, ref_vertical]    // distàncies (horitzontal i vertical) des del punt superior esquerre de la icona fins al punt de localització
+}); 
+
+
+
+
+
+
+
+
 window.onload = () => { 
     let base_de_dades = storage.getItem("base_de_dades");   
     if(base_de_dades == null) {
@@ -26,14 +49,29 @@ window.onload = () => {
                 document.getElementById("desa").style.display = "unset";
             }
         }
+        mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
+        }).addTo(mapa);    // s'afegeix la capa al mapa
+        let vegueries = [[41.39, 2.17, "Àmbit metropolità (Barcelona)"],    // llista on cada element és una llista amb els valors de latitud, longitud i nom de vegueria com a elements
+                 [42.17, 0.89, "Alt Pirineu i Aran (Tremp)"],
+                 [41.12, 1.24, "Camp de Tarragona (Tarragona)"],
+                 [41.73, 1.83 ,"Comarques centrals (Manresa)"],
+                 [41.98, 2.82, "Comarques gironines (Girona)"],
+                 [41.62, 0.62, "Ponent (Lleida)"],
+                 [40.81, 0.52, "Terres de l'Ebre (Tortosa)"],
+                 [41.35, 1.70, "Penedès (Vilafranca del Penedès"]];
+        for (i in vegueries) {    // per cada element de la llista
+            L.marker([vegueries[i][0], vegueries[i][1]],{title:vegueries[i][2]}).addTo(mapa);
+}
     });
    
-    usuari = storage.getItem("usuari");
-    if (usuari != "" && usuari != null) {
-        inicia_sessio();
-    } else {
-        document.getElementById("seccio_0").style.display = "flex";
-    }
+        usuari = storage.getItem("usuari");
+        if (usuari != "" && usuari != null) {
+            inicia_sessio();
+        } else {
+            document.getElementById("seccio_0").style.display = "flex";
+        }
     
 }
 
@@ -210,6 +248,13 @@ function canvia_seccio(num_boto) {
     if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
         omple_llista();
     }
+    if (num_boto == 4) {
+        mapa.invalidateSize();
+        if (typeof geoID === "undefined") {    // si encara no s'han obtingut les dades de localització del dispositiu
+            navigator.geolocation.watchPosition(geoExit);    // inicia el seguiment de la localització del dispositiu
+        }
+    }
+    
 }
 
 
@@ -239,6 +284,16 @@ function esborra_foto(id) {
                 canvia_seccio(3);    // es recarrega la galeria per tal que ja no mostri la foto esborrada
             };
         };
+    }
+}
+
+function geoExit(posicio){
+    let latitud = posicio.coords.latitude;
+    let longitud = posicio.coords.longitude;
+    if (typeof geoID === "undefined") {    
+        geoID = L.marker([latitud, longitud], {zIndexOffset:100, title:"Usuari"}).addTo(mapa);    // es defineix el marcador  geoID i es situa per sobre dels altres
+    } else {    // primeres dades de localització, es crea el marcador d'usuari 
+        geoID.setLatLng([latitud, longitud]);    // actualització de la posició del marcador d'usuari en el mapa
     }
 }
 
